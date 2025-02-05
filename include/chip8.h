c42dd15df1333d2cfcf2be58d348978353692ca2
Jonnen Chong <jonnenc@gmail.com>
2025-02-04 23:54:13 -0500

    add fontset and change chip8 to be struct

--- include/chip8.h
+++ include/chip8.h
@@ -1,7 +1,30 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
-// Declarations here
+// Dependencies
+#include <stdint.h>
+#include <string.h>
+
+// Constants
+#define MEMORY_SIZE (4096)
+#define STACK_SIZE (48)
+#define FONTSET_LENGTH (80)
+
+// Chip8 struct
+typedef struct {
+  uint16_t memory[MEMORY_SIZE]; // 4KB memory from 0x000 to 0xFFF
+  uint8_t V[16];                // 16 general purpose 8-bit registers V0 to VF
+  uint16_t I;                   // index register, used in memory operations
+  uint16_t pc;                  // program counter
+  uint16_t opcode;              // opcode placeholder
+  uint16_t stack[STACK_SIZE];   // 48 byte stack, handles 24 nested subroutine
+                                // calls in theory
+  uint8_t sp;                   // stack pointer, manages the call stack
+  uint8_t delay_timer;          // set and read, counts to 0 at 60 Hz
+  uint8_t sound_timer;          // read, beeps when non-zero at 60 Hz
+} Chip8;
+
+// Chip8 fontset (0 to F) to be preloaded
+extern const uint8_t chip8_fontset[FONTSET_LENGTH];
 
 #endif // CHIP8_H
-
--- src/chip8.c
+++ src/chip8.c
@@ -1,44 +1,77 @@
 #include "../include/chip8.h"
 
-void init_chip8(void) {
-    // initialize system
-    // memory
-    // registers
-    // stack
-    // graphics
+// Chip8 fontset that includes characters 0 to F, 5 bytes long each (8 x 5
+// pixels)
+const uint8_t chip8_fontset[FONTSET_LENGTH] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+/*
+ * This function initializes a Chip8 system specified by the first argument. The
+ * font is stored as sprites from (0x50 to 0x9F).
+ */
+
+void init_chip8(Chip8 *chip8) {
+  // Clear out the garbage values and set them to all-bits-zero
+  memset(chip8, 0, sizeof(Chip8));
+
+  // Preload the fontset into memory from 0x50 to 0x9F
+  for (int i = 0; i < FONTSET_LENGTH; i++) {
+    chip8->memory[i + 0x50] = chip8_fontset[i];
+  }
+
+  // initialize system
+  // memory
+  // registers
+  // stack
+  // graphics
 }
 
-void load_rom(char *filename) {
-    // read file into memory
-    // starting from location 512Kb
-    // or 0x200 in hex
+void load_rom(Chip8 *chip8, char *rom_path) {
+  // read file into memory
+  // starting from location 512Kb
+  // or 0x200 in hex
 }
 
-void execute_opcode() {
-    // fetch opcode
-    opcode = (memory[pc] << 8) + memory[pc + 1];
-    // decode opcode
-    switch (opcode & 0xF000) {
-        // execute opcode
-        case 0x0:
-            if (opcode == 0x00E0)
-                clear_display();
-            elseif (opcode == 0x00EE)
-                // return from subroutine
-        case 0x1:
-        /* . */
-        /* . */
-        /* . */
-        case 0xF:
-    }
+/* void execute_opcode() { */
+/*     // fetch opcode */
+/*     opcode = (memory[pc] << 8) + memory[pc + 1]; */
+/*     // decode opcode */
+/*     switch (opcode & 0xF000) { */
+/*         // execute opcode */
+/*         case 0x0: */
+/*             if (opcode == 0x00E0) */
+/*                 clear_display(); */
+/*             elseif (opcode == 0x00EE) */
+/*                 // return from subroutine */
+/*         case 0x1: */
+/*         /1* . *1/ */
+/*         /1* . *1/ */
+/*         /1* . *1/ */
+/*         case 0xF: */
+/*     } */
 
-    // timers
-    if (delay_timer > 0)
-        --delay_timer;
-    if (sound_timer > 0) {
-        if (sound_timer == 1)
-            play_sound();
-        --sound_timer;
-    }
-}
-
+/*     // timers */
+/*     if (delay_timer > 0) */
+/*         --delay_timer; */
+/*     if (sound_timer > 0) { */
+/*         if (sound_timer == 1) */
+/*             play_sound(); */
+/*         --sound_timer; */
+/*     } */
+/* } */
